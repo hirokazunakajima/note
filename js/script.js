6b3d1e37e18f55c2e008f68277693f4e7f4f7e26
HirokazuNakajima <hirokazu.nakajima.jp@gmail.com>
2014-11-24 20:59:06 -0800

    capable of multi post-it dragging on browser

--- js/script.js
+++ js/script.js
@@ -1,22 +1,13 @@
 /*
-*   drag able contents
+*   drag able contents ( not for IE now )
 */
 
 var root = document.documentElement;
-var el = document.getElementById('box1');
-var rect = el.getBoundingClientRect();
-document.body.appendChild(el);
-
-var left = window.pageXOffset || root.scrollLeft;
-var top = window.pageYOffset || root.scrollTop;
-
-el.style.left = (rect.left + left) + 'px';
-el.style.top = (rect.top + top) + 'px';
-
 var dragging = false;
-var x,y;
+var x,y,dy,dx;
+var el;
 
-// think of the extend able function for multi post it
+// mainly this function will is used to remove 'mousemove' envet as much as possible.
 var addEvent = function(node,type,listener){
     node.addEventListener(type,listener,false);
 }
@@ -34,43 +25,62 @@ addEvent(document,'mouseup',function(e){
 });
 
 
-el.addEventListener('mousedown', function(e){
+var left = root.scrollLeft;
+var top = root.scrollTop;
 
-    if(e.target === el)
-    {
-        dragging = true;
-        x = e.offsetX || e.layerX; // keep current position
+function getStyle(el){
+    // get style (we can get result of that which type of style is assigned to target element)
+    return window.getComputedStyle(el,'');
+}
+
+addEvent(document,'mousedown',function(e){
+
+    var target = e.target;
+    if(target.className == 'wrapper'){
+        x = e.offsetX || e.layerX; //webkit || moz
         y = e.offsetY || e.layerY;
         dragging = true;
-
+        el = target;
+        var style = getStyle(el);
         var rect = el.getBoundingClientRect();
-        var left = window.pageXOffset || root.scrollLeft;
-        var right = window.pageYOffset || root.scrollTop;
+        var left = root.scrollLeft;
+        var top = root.scrollTop;
 
-        el.style.left = left + rect.left + 'px';
-        el.style.top = top + rect.top + 'px';
-        el.style.position = 'absolute';
+        if(style.position === 'fixed'){
+            fixed = true;
+            dx = 0;
+            dy = 0;
+        }else{
+            fixed = false;
+            dx = (rect.left + left) - el.offsetLeft;
+            dy = (rect.top + top) - el.offsetTop;
+        }
+
+        addEvent(document,'mousemove',mousemove);
     }
 
-},false);
+});
 
 
-document.addEventListener('mouseup',function(e){
-    dragging = false;
-    var rect = el.getBoundingClientRect();
-    el.style.left = rect.left + 'px';
-    el.style.top = rect.top + 'px';
-    el.style.position = 'fixed';
-},false);
+addEvent(document,'mouseup',function(e){
+    if(dragging){
+        dragging = false;
+        removeEvent(document,'mousemove',mousemove);
+        var rect = el.getBoundingClientRect();
+        el.style.left = rect.left + 'px';
+        el.style.top = rect.top + 'px';
+        el.style.position = 'fixed';
+    }
+});
 
 
 function mousemove(e){
     if(dragging)
     {
-        var left = window.pageXOffset || root.scrollLeft;
-        var top  = window.pageYOffset || root.scrollTop;
-        el.style.left = left + e.clientX - x + 'px';
-        el.style.top = top + e.clientY - y + 'px';
+        var left = fixed ? 0: root.scrollLeft;
+        var top  = fixed ? 0 : root.scrollTop;
+        el.style.left = left + e.clientX - x - dx+ 'px';
+        el.style.top = top + e.clientY - y -dx + 'px';
 
     }
 }
