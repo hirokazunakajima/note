9fafc92122e8d634d174dd54a6ebf12b368e8212
HirokazuNakajima <hirokazu.nakajima.jp@gmail.com>
2014-12-12 16:54:12 -0800

    + class name change, storage related bug fix

--- js/script.js
+++ js/script.js
@@ -11,9 +11,22 @@ window.onload = function () {
     if (isLocalStorage()) {
         console.log('localStorage OK');
 
+        // load saved data last time
+        for(var i =0;i<localStorage.length;i++)
+        {
+            var pattern = /^note-\d+$/;
+            if(pattern.test(localStorage.key(i))){
+                console.log(localStorage.key(i));
+            };
+        }
+
     } else {
         console.log('localStorage NO');
     }
+
+
+
+
 }
 
 function isLocalStorage() {
@@ -95,7 +108,7 @@ addEvent(document, 'mousedown', function (e) {
     var targetNote = findParent(e, 'wrapper');
     if (targetNote) {
         // change the z-index order of elements
-        if (list.length > 1) bringToFront(targetNote);
+        if (noteList.length > 1) bringToFront(targetNote);
     }
 
 });
@@ -120,7 +133,9 @@ addEvent(document, 'mouseup', function (e) {
         if (textArea.value !== '') {
 
             // save textarea data as JSON format
+
             var dataObj = {
+                'id':targetNote.id,
                 'top': targetNote.style.top,
                 'left': targetNote.style.left,
                 'width': s.width,
@@ -132,36 +147,37 @@ addEvent(document, 'mouseup', function (e) {
             localStorage.setItem(targetNote.id, JSON.stringify(dataObj));
 
             var undoArea = createElement('DIV');
-            setAttributes(undoArea, {'class': 'undo-wrapper', 'data-id': targetNote.id});
+            undoArea.setAttribute('class','undo-wrapper');
+//            setAttributes(undoArea, {'class': 'undo-wrapper', 'data-id': targetNote.id});
             undoArea.style.width = s.width;
             undoArea.style.height = s.height;
             var offset = 20;
             undoArea.style.top = (parseInt(targetNote.style.top) + offset).toString() + 'px';
             undoArea.style.left = (parseInt(targetNote.style.left) + offset).toString() + 'px';
-            var span = createElement('SPAN');
-            var textUndo = document.createTextNode('UNDO');
-            span.setAttribute('class', 'undo-text');
-            span.appendChild(textUndo);
-            undoArea.appendChild(span);
 
-            var deleteBtn = createElement('SPAN');
-            var textX = document.createTextNode('x');
-            setAttributes(deleteBtn, {'class': 'delete-btn', 'id': targetNote.id}); // use the same id as note to delete button, this gonna be key for local strage
-            deleteBtn.appendChild(textX);
-            undoArea.appendChild(deleteBtn);
+            var undoButton = createElement('button');
+            var textUndo = document.createTextNode('UNDO');
+            setAttributes(undoButton, {'class': 'undo-button', 'data-id': targetNote.id});
+
+            undoButton.appendChild(textUndo);
+            undoArea.appendChild(undoButton);
+
+            var deleteButton = createElement('button');
+            var textDelete = document.createTextNode('DELETE');
+            setAttributes(deleteButton, {'class': 'delete-btn', 'id': targetNote.id}); // use the same id as note to delete button, this gonna be key for local strage
+            deleteButton.appendChild(textDelete);
+            undoArea.appendChild(deleteButton);
 
             document.querySelector('body').appendChild(undoArea); // add undo area
 
-            undoAreaList.push(undoArea);
+//            undoAreaList.push(undoArea);
 
-            undoArea.addEventListener('click', undo);
-            undoArea.addEventListener('click', deleteUndo);
+            undoButton.addEventListener('click', undoData);
+            deleteButton.addEventListener('click', deleteData);
         }
 
         document.querySelector('body').removeChild(targetNote);
-        list.splice(list.indexOf(targetNote), 1); // remove element from list
-
-
+        noteList.splice(noteList.indexOf(targetNote), 1); // remove element from noteList
 
     }
 });
@@ -176,31 +192,32 @@ function mousemove(e) {
     }
 }
 
-function undo(e) {
+function undoData(e) {
 
-    if (e.target.className == 'undo-wrapper') {
+    if (e.target.className == 'undo-button') {
 
-        // Undo Note Pad
-        var undoArea = e.target;
-        var dataId = undoArea.getAttribute('data-id');
+        var undoButton = e.target;
+        var dataId = undoButton.getAttribute('data-id');
         var d = JSON.parse(localStorage.getItem(dataId));
-        createNote(d.top, d.left, d.width, d.height, d.text);
+        createNote(d.id, d.top, d.left, d.width, d.height, d.text);
 
-        document.querySelector('body').removeChild(undoArea); // remove undo area
+        var targetNote = findParent(e, 'undo-wrapper');
+        document.querySelector('body').removeChild(targetNote); // remove undo area
     }
 }
 
-function deleteUndo(e) {
+function deleteData(e) {
 
     if (e.target.className == 'delete-btn') {
 
         // remove undo area including undo text
         var undoWrapper = findParent(e, 'undo-wrapper');
         document.querySelector('body').removeChild(undoWrapper); // remove undo area
-        undoAreaList.splice(undoAreaList.indexOf(undoWrapper), 1);
+//        undoAreaList.splice(undoAreaList.indexOf(undoWrapper), 1);
         localStorage.removeItem(e.target.id); // remove data from local storage
     }
 }
+
 /******************
 
  generate note
@@ -208,31 +225,35 @@ function deleteUndo(e) {
  *******************/
 
 var btn = document.querySelector('#generate');
-var list = [];
-var undoAreaList = [];
+var noteList = [];
+//var undoAreaList = [];
 btn.addEventListener('click', function (e) {
     createNote();
 });
 
-function createNote(top, left, w, h, text) {
+function createNote(id , top, left, w, h, text) {
     var note = createElement('DIV');
     var section = createElement('SECTION');
     var textarea = createElement('TEXTAREA');
 
     // create note with params, this will be called from UNDO action
-    if (top && left && w && h && text) {
+    if (id && top && left && w && h && text) {
+        note.setAttribute('id', id);
         note.style.top = top;
         note.style.left = left;
         textarea.style.width = w;
         textarea.style.height = h;
         textarea.value = text;
+    }else{
+        // initial creation of note
+        note.setAttribute('id', 'note-' + noteList.length);
     }
 
     var span = createElement('SPAN');
-    var textX = document.createTextNode('x');
+    var textX = document.createTextNode('-');
 
     note.setAttribute('class', 'wrapper');
-    note.setAttribute('id', 'post-it-' + list.length);
+
     section.setAttribute('class', 'container');
     span.setAttribute('class', 'close-btn');
 
@@ -241,22 +262,22 @@ function createNote(top, left, w, h, text) {
     span.appendChild(textX);
 
     note.appendChild(section);
-    list.push(note);
-    note.style.zIndex = list.length;
+    noteList.push(note);
+    note.style.zIndex = noteList.length;
     document.querySelector('body').appendChild(note);
 }
 
 // Bring obj you clicked to front
-// 1. remove clicked note from list and push to the same list again (change the order)
-// 2. set z-index again follow list array order to move note clicked to top z-index
+// 1. remove clicked note from noteList and push to the same noteList again (change the order)
+// 2. set z-index again follow noteList array order to move note clicked to top z-index
 function bringToFront(clickedObj) {
 
-    for (var i = 0; i < list.length; i++) {
-        if (list[i].id == clickedObj.id) {
-            list.push(list.splice(i, 1)[0]);
+    for (var i = 0; i < noteList.length; i++) {
+        if (noteList[i].id == clickedObj.id) {
+            noteList.push(noteList.splice(i, 1)[0]);
 
-            for (i = 0; i < list.length; i++) {
-                list[i].style.zIndex = i;
+            for (i = 0; i < noteList.length; i++) {
+                noteList[i].style.zIndex = i;
             }
             break;
         }
